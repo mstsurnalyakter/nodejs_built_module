ac75741df3f780ff2c8ec6204913f4a65ee02bf2
Mst Surnaly Akter <surnalyms@gmail.com>
2023-10-02 11:22:01 +0600

    os module

---
+++ os.jsx
@@ -0,0 +1,9 @@
+const os = require('os');
+
+console.log(os.platform());
+console.log(os.arch());
+console.log(os.userInfo());
+console.log(os.homedir());
+console.log(os.hostname());
+console.log(os.totalmem());
+console.log(os.freemem());
