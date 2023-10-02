58507e8bfb918e424c0afa3c2797c9fc1be704c3
Mst Surnaly Akter <surnalyms@gmail.com>
2023-10-02 11:30:44 +0600

    path module

---
+++ path.jsx
@@ -0,0 +1,7 @@
+const path = require('path');
+console.log(path);
+console.log(__dirname);
+console.log(__filename);
+console.log(path.join(__dirname + "/views"));
+console.log(path.join(__dirname + "/../views"));
+console.log(path.join(__dirname + "/../../views"));
